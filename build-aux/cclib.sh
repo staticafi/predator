74b543fdd53d90e9bf084bceddee2366d334e914
Marek Chalupa <mchqwerty@gmail.com>
2020-11-25 13:44:57 +0100

    cclib.sh: search also installed paths

--- build-aux/cclib.sh
+++ build-aux/cclib.sh
@@ -46,5 +46,6 @@ find_opt_host() {
 
 find_plug() {
     test -r "${!1}" || eval $1="$topdir/${2}_build/lib${2}.so"
+    test -r "${!1}" || eval $1="$topdir/lib/lib${2}.so"
     test -r "${!1}" || die "$3 plug-in not found: ${!1}"
 }
